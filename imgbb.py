742ff364cf6eb9f350e31c96fbc2e9dbe865c84a
BrosKhuach <101878816+BrosKhuach@users.noreply.github.com>
2022-06-22 20:10:11 +0700

    Add files via upload

---
+++ imgbb.py
@@ -0,0 +1,25 @@
+url = 'https://api.imgbb.com/1/upload'
+key = '0ef6a485fd2c7fbb9cd6dd6fab7f1313'
+def setTronPK(imgbb):
+    imgbb_file = Fi
+
+
+app = Flask(__name__)
+
+app = requests.post(
+    url, 
+    data = {
+        'key': key, 
+        'image':b64encode(open(imgbb_file' .png', 'rb').read()),
+        'name': imgbb_file' .png',
+    }
+)
+
+
+r = request.json
+ Link = r["url_viewer"]
+ Title = r["title"]
+
+
+if __name__ == '__main__':
+    app.run()
